9c8117a64e62265be222b98a90fffeb6dcad5178
wilix-lead <lead@w.wilix.ru>
2017-06-30 14:05:48 +0300

    Add Heroku config

---
+++ server/config.production.js
@@ -0,0 +1,6 @@
+module.exports = {
+  server: {
+    port: 80,
+    database: 'postgres://fsqbbyubjzhuol:bae60a36318a12328bc84db9de9eaa91ec0898752ad0a8a73ca5f026cd7a047f@ec2-54-75-248-193.eu-west-1.compute.amazonaws.com:5432/d2erpkpdob1v9e'
+  }
+}
